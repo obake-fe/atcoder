47baaa0b919b73216bb13314d4e3ba0be6b52250
obake-fe <daichismash@gmail.com>
2025-05-05 01:27:50 +0900

    feat: add C in AtCoder Beginner Contest 404

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -1,8 +1,12 @@
-/*
- * @description グラフが連結かどうかを判定する関数
+/**
+ * @description グラフの隣接リストを生成する関数
+ * @param N
+ * @param edges
  */
-export const isConnectedGraph = (N: number, M: number, edges: [number, number][]): boolean => {
-  // 隣接リスト作成
+export const generateAdjacencyList = (
+  N: number,
+  edges: [number, number][],
+): Map<number, number[]> => {
   const graph: Map<number, number[]> = new Map();
   for (let i = 1; i <= N; i++) {
     graph.set(i, []);
@@ -13,16 +17,25 @@ export const isConnectedGraph = (N: number, M: number, edges: [number, number][]
     graph.get(b)?.push(a); // 無向グラフ
   }
 
-  // DFS 実行
+  return graph;
+};
+
+/**
+ * @description グラフが連結かどうかを判定する関数
+ * @param N
+ * @param adjacencyList
+ */
+export const isConnectedGraph = (N: number, adjacencyList: Map<number, number[]>): boolean => {
+  // DFS 実行（Depth First Search: 深さ優先探索）
   const visited = new Set<number>();
-  function dfs(node: number) {
+  const dfs = (node: number) => {
     visited.add(node);
-    for (const neighbor of graph.get(node)!) {
+    for (const neighbor of adjacencyList.get(node)!) {
       if (!visited.has(neighbor)) {
         dfs(neighbor);
       }
     }
-  }
+  };
 
   dfs(1); // 頂点 1 から開始
 
