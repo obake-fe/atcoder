f38bb1b17a8794e0e2b16b3bcf3cff0132badce9
obake-fe <daichismash@gmail.com>
2025-05-06 23:32:11 +0900

    feat: add practice B52

---
+++ src/practice/B52/index.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from 'fs';
+const isLocal = process.env.LOCAL === 'true'; // 環境変数をチェック
+
+const input = isLocal
+  ? readFileSync(`src/practice/B52/text.txt`, 'utf8').trim() // ローカル用
+  : readFileSync('/dev/stdin', 'utf8').trim(); // 本番用
+
+const lines = input.split('\n');
+const [N, X] = lines[0].split(' ').map(Number);
+const balls = lines[1].split('');
+
+const queue: number[] = [X - 1]; // 位置Xを index に変換
+balls[X - 1] = '@';
+
+while (queue.length > 0) {
+  const pos = queue.shift()!;
+
+  // 左チェック
+  if (pos > 0 && balls[pos - 1] === '.') {
+    balls[pos - 1] = '@';
+    queue.push(pos - 1);
+  }
+
+  // 右チェック
+  if (pos < N - 1 && balls[pos + 1] === '.') {
+    balls[pos + 1] = '@';
+    queue.push(pos + 1);
+  }
+}
+
+console.log(balls.join(''));
